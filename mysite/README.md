d1711b3ed185e3720e54834d0201f8e8079e49b6
JaeHyeon Son <sjh9391985@gmail.com>
2021-07-19 17:04:32 +0900

    git con

--- mysite/README.md
+++ mysite/README.md
@@ -2,8 +2,10 @@
 
 ## 설치 패키지
 $ npm init -y<br>
+
 $ npm i express<br>
 $ npm i express-session<br>
+$ npm i serve-favicon<br>
 $ npm i ejs<br>
 $ npm i dotenv<br>
 $ npm i sequelize<br>
@@ -12,7 +14,10 @@ $ npm i multer<br>
 $ npm i moment<br>
 $ npm i winston<br>
 $ npm i winston-daily-rotate-file<br>
+
 $ npm i -D nodemon<br>
+$ npm i -D mocha<br>
+$ npm i -D chai<br>
 
 ## script in package.json setting
 ''' JSON '''
@@ -20,36 +25,41 @@ $ npm i -D nodemon<br>
 ''' 
     "scripts": 
     {
-    "start" : "node index.js",
-    "debug" : "nodemon index.js"
+    "start": "node index.js",
+    "test": "npx mocha",
+    "debug": "nodemon index.js"
     }
 
 ## project structure [project 구조는 개발자의 자유]
 <pre>
 /mysite
-    |---- index.js
-    |---- pacakge.json
-    |---- package-lock.json
-    |---- /node-modules
-    |---- /config
-    |---- /public
-              |--- /assets
-                       |--- /css
-                       |--- /js
-                       |--- /image
-                       |--- /gallery
-    |---- /routes
-    |---- /controllers
-    |---- /logging
-    |---- /logs
-    |---- /multer-temporary-store
-    |---- /models
-    |---- /views
-            |---- /main
-            |---- /user
-            |---- /guestbook
-            |---- /board
-            |---- /gallery
-            |---- /admin
-            |---- /error
+    |--- index.js
+    |--- package.json
+    |--- package-lock.json
+    |--- [node_modules]
+    |--- test
+    |--- logging
+    |--- [logs]
+    |       |--- [error]
+    |--- [multer-temporary-store]
+    |--- config
+    |--- public
+    |       |--- assets
+    |               |--- js
+    |               |--- css
+    |               |--- images
+    |               |--- [upload-images]
+    |--- routes
+    |--- controllers
+    |       |--- admin
+    |--- models
+    |--- views
+            |--- main
+            |--- admin
+            |       |--- includes
+            |--- user
+            |--- board
+            |--- guestbook
+            |--- gallery
+            |--- includes
 </pre>
--- mysite/index.js
+++ mysite/index.js
@@ -1,3 +1,4 @@
+(function(){
 const express = require('express'); //require는 동기방식이기 때문에 코드의 중간에 하면 서버가 돌때 문제가 생길수 있다.(맨앞에 하는것을 권장)
 const session = require('express-session');
 const http = require('http');
@@ -5,91 +6,95 @@ const path = require('path');
 const dotenv = require('dotenv');
 const multer = require('multer');
 
-// Environment Variable(환경변수)
+// 1. Environment Variable(환경변수)
 dotenv.config({ path: path.join(__dirname, 'config/app.env') })
 dotenv.config({ path: path.join(__dirname, 'config/db.env') })
 
-const mainRouter = require('./routes/main');
-const userRouter = require('./routes/user');
-const guestbookRouter = require('./routes/guestbook');
-const errorRoute = require('./routes/error');
-const galleryRouter = require('./routes/gallery')
-const boardRouter = require('./routes/board')
+// 2. Application Routers
+const { applicationRouter } = require('./routes');
+// const mainRouter = require('./routes/main');
+// const userRouter = require('./routes/user');
+// const guestbookRouter = require('./routes/guestbook');
+// const errorRoute = require('./routes/error');
+// const galleryRouter = require('./routes/gallery')
+// const boardRouter = require('./routes/board')
+// // api-router
+// const userApiRouter = require('./routes/user-api');
+// const guestbookApiRouter = require('./routes/guestbook-api');
 
-// api-router
-const userApiRouter = require('./routes/user-api');
-const guestbookApiRouter = require('./routes/guestbook-api');
-
-// Logging
+// 3. Logging
 const logger = require('./logging');
 
 
-// Application Setup
+// 4. Application Setup
 const application = express()
-    // 1. session enviroment
+    // 4-1. session enviroment
     .use(session({
         secret: 'mysite-session', // 쿠키 변조를 방지하기 위한 값.
         resave: false,  // req.session.authUser= user (요청처리에서 세션이 변경 사항이 없어도 항상 저장)
         saveUninitialized: false    // 새로 세션을 생성할 때 "uninitialized" 상태로 둔다. 따라서 로그인 세션처리에서는 false로 해주면 좋다.
     }))
 
-    // 2. request body parser
+    // 4-2. request body parser
     .use(express.urlencoded({extended: true})) //application/x-www-form-urlencoded
     .use(express.json())                       //application/json
 
-    // 3. multipart
+    // 4-3. multipart
     .use(multer({
         dest: path.join(__dirname, process.env.MULTER_TEMPORARY_STORE)
 
     }).single('file'))
 
-    // 4. static serve(체인걸음)
+    // 4-4. static serve(체인걸음)
     .use(express.static(path.join(__dirname, process.env.STATIC_RESOURCES_DIRECTORY)))
     
-    // 5. view engine setup
+    // 4-5. view engine setup
     .set("views", path.join(__dirname, "views"))
     .set('view engine', 'ejs')
-
-    // 6. request router
-    .all('*', function(req,res, next){ // 모든 메소드의 url 설정
-        res.locals.req = req;
-        res.locals.res = res;
-        next();
-    })
-    .use('/', mainRouter) // < - 이부분에서 계속 추가
-    .use('/user', userRouter) 
-    .use('/api/user', userApiRouter) 
     
-    .use('/guestbook', guestbookRouter)
-    .use('/api/guestbook', guestbookApiRouter)
+// 5. Application Router Setup
+applicationRouter.setup(application);
+    // // 6. request router
+    // .all('*', function(req,res, next){ // 모든 메소드의 url 설정
+    //     res.locals.req = req;
+    //     res.locals.res = res;
+    //     next();
+    // })
+    // .use('/', mainRouter) // < - 이부분에서 계속 추가
+    // .use('/user', userRouter) 
+    // .use('/api/user', userApiRouter) 
+    
+    // .use('/guestbook', guestbookRouter)
+    // .use('/api/guestbook', guestbookApiRouter)
 
-    .use('/gallery', galleryRouter)
+    // .use('/gallery', galleryRouter)
 
-    .use('/board' , boardRouter)
+    // .use('/board' , boardRouter)
 
-    .use(errorRoute.error404) //404 에러처리
-    .use(errorRoute.error500) //500 처리
+    // .use(errorRoute.error404) //404 에러처리
+    // .use(errorRoute.error500) //500 처리
 
-// server setup
+// 6. server setup
 http.createServer(application)
-    .on('listening', function(){
-        logger.info(`Http Server running on port ${process.env.PORT}`);
-    })
-    .on('error', function(error){
-        if(error.syscall != 'listen'){
-            throw error;
-        }
-        switch(error.code){
-            case 'EACCESS':
-                logger.error(`Port: ${process.env.PORT} requires privileges`); //port를 열지 못햇을때
-                process.exit(1); // 1번은 비정상종료
-                break;
-            case 'EADDRINUSE' :
-                logger.error(`Port: ${process.env.PORT} is already in use`); //port를 이미 사용하고 있을때
-                process.exit(1); 
-                break;
-            default:
+        .on('listening', function(){
+            logger.info('Listening on port ' + process.env.PORT );
+        })
+        .on('error', function(error) {
+            if(error.syscall !== 'listen') {
                 throw error;
-        }
-    })
-    .listen(process.env.PORT);
+            }
+            switch(error.code) {
+                case 'EACCES':
+                    logger.error('Port ' + process.env.PORT  + ' requires elevated privileges');
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    logger.error('Port ' + process.env.PORT  + ' is already in use');
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        })
+        .listen(process.env.PORT);
+})();
